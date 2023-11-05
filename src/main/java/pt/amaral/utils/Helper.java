64c8c8064aa0b1e74b2384fbaadea4f44779abbe
João Amaral <amaral.joao@ua.pt>
2023-11-05 19:56:07 +0000

    Fix missing space

--- src/main/java/pt/amaral/utils/Helper.java
+++ src/main/java/pt/amaral/utils/Helper.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.util.Map;
 
 public class Helper {
-    public static Map<String, Object>   serilizeResponse(String response) throws JsonProcessingException {
+    public static Map<String, Object> serilizeResponse(String response) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
 
