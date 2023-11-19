2e53ef3bdc39ef0d95b9f88e759d94b336cc2621
João Luís Amaral <amaral.joao@ua.pt>
2023-11-19 14:56:51 +0000

    9-backend-create-the-docker-image

--- bin/configurator.sh
+++
@@ -1,7 +0,0 @@
-input_file="application.properties.SAMPLE"
-values_file="configurator.properties"
-
-output_file=$(echo "$input_file" | sed 's/\.SAMPLE//')
-
-awk -F= 'NR==FNR {a[$1]=$2; next} {while (match($0, /{{([^}]*)}}/)) { key=substr($0, RSTART+2, RLENGTH-4); if(key in a) $0=substr($0, 1, RSTART-1) a[key] substr($0, RSTART+RLENGTH); else $0=substr($0, 1, RSTART-1) "{{"key"}" substr($0, RSTART+RLENGTH) } } 1' "$values_file" "$input_file" > "$output_file"
-
---
+++ buildFiles/createImage.sh
@@ -0,0 +1,13 @@
+docker build \
+  --build-arg DB_USER_NAME=${DB_USER_NAME} \
+  --build-arg DB_USER_PASSWORD=${DB_USER_PASSWORD} \
+  --build-arg DB_HOST_URL=${DB_HOST_URL} \
+  --build-arg DB_DATABASE=${DB_DATABASE} \
+  --build-arg DB_SCHEMA=${DB_SCHEMA} \
+  --build-arg GELF_ENABLE=${GELF_ENABLE} \
+  --build-arg GELF_HOST=${GELF_HOST} \
+  --build-arg GELF_PORT=${GELF_PORT} \
+  --build-arg GELF_FACILITY=${GELF_FACILITY} \
+  -f $PWD/docker/Dockerfile \
+  -t quarkus/tv-channel-service \
+  .
