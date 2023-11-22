2d0785e37d48e6e465cbafd166e4869532c176ec
João Amaral <amaral.joao@ua.pt>
2023-11-22 23:52:47 +0000

    9-backend-create-the-docker-image

--- buildFiles/createImage.sh
+++ buildFiles/createImage.sh
@@ -1,4 +1,45 @@
-docker build \
-  -f $PWD/docker/Dockerfile-backend \
-  -t quarkus/tv-channel-service \
-  ..
+MODULES=("./docker/Dockerfile-backend")
+
+#print_usage
+print_usage()
+{
+    echo "options are:"
+    echo "-p to push images default false"
+    echo "-m modules separated by commam default are ${ALL_MODULES[@]}"
+}
+
+while getopts 'pm:e:h' flag; do
+  case "${flag}" in
+    p) PUSH='true' ;;
+    m) MODULES=(${OPTARG}) ;;
+    h) print_usage
+        exit 1 ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+create_image():wq
+{
+    start='date +%s'
+
+    MODULE='echo $1 |sed  "s/.*docker\/Dockerfile-//"'
+    IMAGE_NAME=${MODULE}
+
+    docker build \
+      -f $PWD/docker/Dockerfile-backend \
+      -t tv-channel-service:1.0.0 \
+      ..
+
+    if [ $PUSH = "true" ]; then
+        docker tag tv-channel-service:1.0.0 10.10.0.222:5000/tv-channel-service:1.0.0
+        docker push 10.10.0.222:5000/tv-channel-service:1.0.0
+    fi
+    end="date +%s"
+    echo "$IMAGE_NAME took: $( echo "$end - $start" | bc -l )s"
+}
+
+for ITEM in "${MODULES[@]}"; do
+    echo "creating $ITEM";
+    create_image $ITEM
+done
--- src/backend/target/classes/db/1.0.0/V1.0.0_2__create_custom_show_type.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE cat_custom_shows_types (
-    name text PRIMARY KEY,
-    custom_type text
-);
